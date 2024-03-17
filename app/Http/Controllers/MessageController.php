9c2a714d62ff32b25659fabcc7b34ab027fe2956
saktiworkstation <saktiworkstation@gmail.com>
2024-03-17 13:55:21 +0700

    Create Message Controller

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function create(){
+        // untuk halaman create message
+    }
+    public function send(){
+        // untuk submit message
+    }
+    public function list(){
+        // untuk menampilkan semua halaman messsage
+    }
+}
