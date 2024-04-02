16f3baca2d4d204eabddd6569f99390411a2f143
saktiworkstation <saktiworkstation@gmail.com>
2024-04-02 20:23:34 +0700

    edit project migrations

--- database/migrations/2024_03_31_133908_create_projects_table.php
+++ database/migrations/2024_03_31_133908_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('img');
+            $table->string('link');
             $table->timestamps();
         });
     }
