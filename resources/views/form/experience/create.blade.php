27b2fdee2e0c19cf4d42aa586e5853f7ed5ef511
saktiworkstation <saktiworkstation@gmail.com>
2024-04-22 21:45:42 +0700

    fixed some

--- resources/views/form/experience/create.blade.php
+++ resources/views/form/experience/create.blade.php
@@ -68,9 +68,9 @@
                         @enderror
                     </div>
 
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Create</button>
                 </form>
-                <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register now!</a></small>
+                <small class="d-block text-center mt-3">Not Registered? <a href="/dashboard">Back to dashboard</a></small>
             </main>
         </div>
     </div>
