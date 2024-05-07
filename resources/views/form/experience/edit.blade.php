222a0d3351e7715720989dd38225e4ce0b3164ad
RasyifIbnu <156682958+RasyifIbnu@users.noreply.github.com>
2024-05-07 16:37:25 +0700

    update edit blade

--- resources/views/form/experience/edit.blade.php
+++ resources/views/form/experience/edit.blade.php
@@ -1,5 +1,79 @@
 @extends('form.main')
 
 @section('container')
-    {{-- Isi disini --}}
+    {<div class="row justify-content-center mt-5">
+        <div class="col-md-4 ">
+
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
+            <main class="form-signin">
+                <h1 class="h3 mb-3 fw-normal text-center">Edit experience</h1>
+                <form action="/dashboard/form/experience/edit" method="post">
+                    @csrf
+                    <div class="form-floating">
+                        <input type="text" name="company"
+                            class="form-control @error('company')
+                            is-invalid
+                            @enderror"
+                            id="company" placeholder="example inc" value="{{ old('company', $experience->company) }}"
+                            required autofocus>
+                        <label for="company">company</label>
+                        @error('company')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" name="durations"
+                            class="form-control @error('durations')
+                            is-invalid
+                            @enderror"
+                            id="durations" placeholder="30-05-1992 - 12-09-1982"
+                            value="{{ old('durations', $experience->durations) }}" required>
+                        <label for="durations">durations</label>
+                        @error('durations')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" name="field"
+                            class="form-control @error('field')
+                            is-invalid
+                            @enderror"
+                            id="field" placeholder="programmer" value="{{ old('field', $experience->field) }}" required>
+                        <label for="field">field</label>
+                        @error('field')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" name="order"
+                            class="form-control @error('order')
+                            is-invalid
+                            @enderror"
+                            id="order" placeholder="1" value="{{ old('order', $experience->order) }}" required>
+                        <label for="order">order</label>
+                        @error('order')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Create</button>
+                </form>
+                <small class="d-block text-center mt-3"><a href="/dashboard">Back to dashboard</a></small>
+            </main>
+        </div>
+    </div>}
 @endsection
